a4e7d90011369f1fbb439027e27e92e9fabb005f
Marcos Duarte <marcosduarte@Marcoss-MacBook-Air.local>
2021-05-28 20:49:39 -0300

    Finalizando a configuracao do stripe

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss';
 export function SigninButton() {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
     <button 
       type="button"
